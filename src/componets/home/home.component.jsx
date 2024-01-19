385f2d1e0daca8efa3f53ab0e8beabd270d4a998
ambilynnponnu <103410022+ambilynnponnu@users.noreply.github.com>
2024-01-19 21:03:09 +0530

    commit

--- src/componets/home/home.component.jsx
+++ src/componets/home/home.component.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import nature from "../../images/nature.jpg";
-import nature1 from "../../images/nature1.jpg";
-
-import nature2 from "../../images/natur2.jpg";
 
 function Home() {
   return (
     <div className="bg-img">
-      <div>
-        <div>
-          <p className="head_h d-flex justify-content-center">Learn to Code</p></div>
-      </div>
+     
     </div>
   );
 }
--- src/componets/home/navbar.component.jsx
+++ src/componets/home/navbar.component.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-
-
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <div>
-      <nav className="navbar home-page-navbar" >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">HOME</a>
-          <a className="navbar-brand" onClick={()=>navigate('/Login')}>Login</a>
-          {/* <a className="navbar-brand" onClick={()=>navigate('/Todo')}>Todo</a> */}
-
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+              <a class="nav-link" href="#">
+               Signup
+              </a>
+              <a class="nav-link" onClick={()=>navigate("/Login")}>
+               Login
+              </a>
+              
+            </div>
+          </div>
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
