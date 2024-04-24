e2c9f4e4162f719af8b7e3cb38295d2608670190
sethulakshmy24 <sethu29286@gmail.com>
2024-04-24 20:24:50 +0530

    add api support with filters

--- server/index.js
+++ server/index.js
@@ -9,6 +9,55 @@ const readline = require("readline");
 const fs = require("fs");
 var jsonParser = bodyParser.json();
 var jsonParserForFeedback = bodyParser.json({ limit: "100mb" });
+// Define your list of products with details
+const products = [
+  {
+    name: "Cabbage",
+    image: "https://www.freepngimg.com/thumb/categories/2970.png",
+    quantity: "One Unit",
+    id: 2,
+    price: 30,
+    description:
+      "Rich in vitamin C, supporting immune health and collagen production,Excellent source of vitamin, promoting bone health and blood clotting, Low in calories and high in fiber, aiding in digestion and weight management.",
+    category: "vegetable",
+  },
+  {
+    name: "Capsicum",
+    image:
+      "https://www.nicepng.com/png/detail/52-525615_green-bell-pepper-png-green-capsicum-png.png",
+    quantity: "One Unit",
+    id: 7,
+    price: 50,
+    category: "vegetable",
+  },
+  {
+    name: "Banana",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fbanana&psig=AOvVaw2xYxk1z-I6CMP9UYTNligW&ust=1713527629518000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDyp4Lay4UDFQAAAAAdAAAAABAE",
+    quantity: "One",
+    id: 10,
+    price: 40,
+    category: "Fruit",
+  },
+  {
+    name: "Mango",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flipkart.com%2Fexotica-glenn-mango-plant%2Fp%2Fitmfec6a29a5e54e&psig=AOvVaw3pN2bkIrnB1bEApRcd13dy&ust=1713527691454000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIibw5_ay4UDFQAAAAAdAAAAABAE",
+    quantity: "One Unit",
+    id: 9,
+    price: 70,
+    category: "Fruit",
+  },
+  {
+    name: "Orange",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Forange-fruit.html&psig=AOvVaw3ogP5Qqndz90I7iXFA5fQW&ust=1713527676188000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODj25Xay4UDFQAAAAAdAAAAABAE",
+    quantity: "One Unit",
+    id: 8,
+    price: 30,
+    category: "Fruit",
+  },
+];
 module.exports = ({ httpPort = 3000 }) => {
   const app = express();
   // Serve static files from the 'build' directory
@@ -135,58 +184,19 @@ module.exports = ({ httpPort = 3000 }) => {
   });
 
   app.get("/products", (req, res) => {
-    // Define your list of vegetables with details
-    const vegetables = [
-      {
-        name: "Cabbage",
-        image: "https://www.freepngimg.com/thumb/categories/2970.png",
-        quantity: "One Unit",
-        id: 2,
-        price: 30,
-        description:
-          "Rich in vitamin C, supporting immune health and collagen production,Excellent source of vitamin, promoting bone health and blood clotting, Low in calories and high in fiber, aiding in digestion and weight management.",
-        category: "vegetable",
-      },
-      {
-        name: "Capsicum",
-        image:
-          "https://www.nicepng.com/png/detail/52-525615_green-bell-pepper-png-green-capsicum-png.png",
-        quantity: "One Unit",
-        id: 7,
-        price: 50,
-        category: "vegetable",
-      },
-      {
-        name: "Banana",
-        image:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fbanana&psig=AOvVaw2xYxk1z-I6CMP9UYTNligW&ust=1713527629518000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDyp4Lay4UDFQAAAAAdAAAAABAE",
-        quantity: "One",
-        id: 10,
-        price: 40,
-        category: "Fruit",
-      },
-      {
-        name: "Mango",
-        image:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flipkart.com%2Fexotica-glenn-mango-plant%2Fp%2Fitmfec6a29a5e54e&psig=AOvVaw3pN2bkIrnB1bEApRcd13dy&ust=1713527691454000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIibw5_ay4UDFQAAAAAdAAAAABAE",
-        quantity: "One Unit",
-        id: 9,
-        price: 70,
-        category: "Fruit",
-      },
-      {
-        name: "Orange",
-        image:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Forange-fruit.html&psig=AOvVaw3ogP5Qqndz90I7iXFA5fQW&ust=1713527676188000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODj25Xay4UDFQAAAAAdAAAAABAE",
-        quantity: "One Unit",
-        id: 8,
-        price: 30,
-        category: "Fruit",
-      },
-    ];
+    const { category } = req.query;
+    console.log("category => ", category)
+    
+    const result =  products.filter(prod => {
+      if(category && prod.category === category) {
+        return true
+      } else if(!category) {
+        return true
+      } else return false
+    })
 
-    // Send the list of vegetables as a JSON response
-    res.json(vegetables);
+    // Send the list of products as a JSON response
+    res.json(result);
   });
 
   // Start the server
